2e306e48c3dc444121cf46edfcca21bf0099568e
Justin B. Yee <jbyee2015@gmail.com>
2025-10-31 00:17:19 -0700

    Removed unnecessary files.

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -18,7 +18,7 @@ from .base import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-DEBUG = False
+DEBUG = True
 
 WSGI_APPLICATION = "config.wsgi.prod.application"
 
