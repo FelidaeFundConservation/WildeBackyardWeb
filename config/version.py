0bd38be58ce700d2a70d06988c976a950760fb28
John A Novak <oldtopos@edgeoftheblade.com>
2026-02-16 17:18:40 -0600

    Add version information for the application

---
+++ config/version.py
@@ -0,0 +1,10 @@
+"""
+Version information for the application.
+This file is automatically updated by the deployment script.
+"""
+
+VERSION = {
+    "commit_hash": "78f968d",
+    "release_tag": "dev",
+    "branch": "main",
+}
